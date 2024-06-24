c37f786ae1faf64a401ccef70bf994cf58a1fd9b
Varun Yadav <vy056038@gmail.com>
2024-06-24 09:41:39 +0530

    created a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import  dotenv from "dotenv";
 import dbConnection from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -10,3 +11,8 @@ dbConnection();
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
 });
+
+// routes (APIs)
+app.use('/api/v1/user',userRoutes);
+
+
