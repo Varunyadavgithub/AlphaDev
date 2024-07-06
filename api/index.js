7a5c39d88b935323d280e87df1e525f141ec089b
Varun Yadav <vy056038@gmail.com>
2024-07-06 18:05:44 +0530

    added comment section to the post page

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import  dotenv from "dotenv";
+import dotenv from "dotenv";
 import dbConnection from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import postRoutes from  "./routes/post.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -12,31 +13,31 @@ dotenv.config();
 dbConnection();
 
 // Middlewares
-app.use(express.urlencoded({
-    extended:true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${process.env.PORT}`);
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
 
 // routes (APIs)
-app.use('/api/v1/user',userRoutes);
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/post',postRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/comment", commentRoutes);
 
 // Middleware to handle the error
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'Internal Server error';
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    })
-})
-
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../Components/CallToAction";
+import CommentSection from "../Components/CommentSection";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -64,6 +65,7 @@ const PostPage = () => {
         <div className="max-w-4xl mx-auto w-full">
           <CallToAction/>
         </div>
+        <CommentSection postId={post._id}/>
       </main>
     </>
   );
